7c1d053554cdc2d80a0fd9e1f2ace462178159eb
TRI HATMOKO <70934860+tirtaandes@users.noreply.github.com>
2023-11-09 13:11:49 +0700

    Add files via upload

--- biodataku/dbbiodata.sql
+++ biodataku/dbbiodata.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2023 at 06:18 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Nov 09, 2023 at 07:09 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,15 +33,17 @@ CREATE TABLE `tbanggota` (
   `alamat` varchar(255) DEFAULT NULL,
   `tanggal_lahir` date DEFAULT NULL,
   `jenis_kelamin` enum('Laki-laki','Perempuan') DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `email` varchar(255) DEFAULT NULL,
+  `foto_path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbanggota`
 --
 
-INSERT INTO `tbanggota` (`id`, `nama`, `alamat`, `tanggal_lahir`, `jenis_kelamin`, `email`) VALUES
-(8, 'riko', 'bunut', '1960-02-01', 'Laki-laki', 'asdasa@gmail.com');
+INSERT INTO `tbanggota` (`id`, `nama`, `alamat`, `tanggal_lahir`, `jenis_kelamin`, `email`, `foto_path`) VALUES
+(12, 'NIKMATUL CHOIRIYAH', 'bahar', '2023-11-08', 'Perempuan', 'esemka6majamSbi@gmail.com', 'uploads/Adopsi-Teknologi-Baru.jpeg'),
+(13, 'TRI HATMOKO', 'bahar', '2023-10-19', 'Laki-laki', 'esemka6majHambi@gmail.com', 'uploads/download.jpg');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +64,7 @@ ALTER TABLE `tbanggota`
 -- AUTO_INCREMENT for table `tbanggota`
 --
 ALTER TABLE `tbanggota`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
